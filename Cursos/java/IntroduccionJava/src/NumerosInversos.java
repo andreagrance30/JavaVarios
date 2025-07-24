eeba815d55b4887dc3de46f66ce8b3f64d9f275e
Andrea <andrenoesagra86@gmail.com>
2025-07-23 22:53:03 -0300

    Se alza ciclo do while de números inversos en fecha 23/07/2025

---
+++ Cursos/java/IntroduccionJava/src/NumerosInversos.java
@@ -0,0 +1,9 @@
+public class NumerosInversos {
+    public static void main(String[] args) {
+        System.out.println("***Sistema de Números Inversos***");
+        var i=10;
+        do {
+            System.out.printf(i-- + " ");
+        }while (i>0);
+    }
+}
